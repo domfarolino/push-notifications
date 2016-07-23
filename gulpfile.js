359f9959e7483515fa901a9a70668b3555157e4c
Dom Farolino <farolidm@mail.uc.edu>
2016-07-23 19:51:30 +0000

    chore(deps): add browser-sync to deps and init in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
 // gulp.task('html', function() {
 //   console.log("HTML Task Run");
 // });
 
-// gulp.task('watch', function() {
-//   gulp.watch('*.html', ['html']);
-// })
+gulp.task('watch', function() {
+  gulp.watch('app/*', ['browserSync']);
+})
