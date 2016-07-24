1b4fff979c34822ed32169af235779b4b7aa9d7a
dom.farolino <dom.farolino@inin.com>
2016-07-23 23:36:00 -0400

    Update 2016-07-24T03:35:59.148Z

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-// gulp.task('html', function() {
-//   console.log("HTML Task Run");
-// });
-
-// gulp.task('watch', function() {
-//   gulp.watch('*.html', ['html']);
-// })
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(request, response, next) {
-  response.send('respond with a resource');
-});
-
-module.exports = router;
