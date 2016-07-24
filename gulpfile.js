9b60e090a5ddf6005f0f0fbafe04c25a524f5c1e
Dom Farolino <domfarolino@gmail.com>
2016-07-23 23:37:30 -0400

    feat(gh-pages): gulp deploy deploys gh-pages branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 
 var browserSync = require('browser-sync').create();
+var ghPages = require('gulp-gh-pages');
+ 
+gulp.task('deploy', function() {
+  return gulp.src('./src/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('browserSync', function() {
   browserSync.init({
