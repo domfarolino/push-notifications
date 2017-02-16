a95bad70e2adb556b0e341fe86da6981b43a113b
Dom Farolino <domfarolino@gmail.com>
2017-02-16 11:09:39 -0500

    Update 2017-02-16T16:09:37.970Z

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
-
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -30,6 +11,7 @@ var AppController = function () {
     _classCallCheck(this, AppController);
 
     this.backendURL = 'https://push-notifications-sw.herokuapp.com';
+    this.backendURL = 'http://localhost:5000';
 
     this.registration = null;
     this.subscription = null;
@@ -128,7 +110,7 @@ var AppController = function () {
   }, {
     key: 'buildValuesFromSubscription',
     value: function buildValuesFromSubscription() {
-      console.log('buildValuesFromSubscription() t');
+      console.log('buildValuesFromSubscription()');
 
       if (this.subscription) {
         this.endpoint = this.subscription.endpoint;
