a10e9f0bd87dbdace4b4d9e2c837ff4dbbeab98b
Dom Farolino <domfarolino@gmail.com>
2017-02-21 08:30:00 -0500

    Update some deps

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
-const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
