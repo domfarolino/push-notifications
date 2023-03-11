78d15e87cd9767d8521d9de54a0ea668466eb56d
Dominic Farolino <domfarolino@gmail.com>
2023-03-11 16:20:12 -0500

    Remove body-parser dependency

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
 const app = express();
@@ -16,8 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
