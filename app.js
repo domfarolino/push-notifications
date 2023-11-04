8db89c84a5f9e3b9cc0be3b7ece87caa1457b9e3
Dominic Farolino <domfarolino@gmail.com>
2023-11-04 16:18:13 -0400

    Remove ejs and vestigial views dependency

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ const express = require('express');
 const routes = require('./routes/index');
 const app = express();
 
-// View engine setup
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -21,26 +16,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+console.log('Creating server');
+require('http').createServer(app).listen(process.env.PORT, _ => {});
+
+// Catch 404 and forward to error handler.
+app.use((request, response, next) => {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-console.log('Creating server');
-require('http').createServer(app).listen(process.env.PORT, _ => {});
-
-// Error handler
-
-// Development error handler
-// Will print stacktrace
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
+app.use((err, request, response, next) => {
+  response.status(err.status || 500);
+  response.send(`${err.status} Error: ${err.message}`);
 });
 
 module.exports = app;
